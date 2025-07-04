79b2adca5a26998ee05e2003bbb322afeb7e305c
lahiruransara <lahiruransara123@gmail.com>
2025-07-03 20:38:10 -0700

    Mobile Nav

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,7 +1,10 @@
 // Selectors
 const themeToggleBtn = document.querySelector('.theme-toggle');
+const themeToggleBtn2 = document.querySelector('.theme-toggle2');
 const lightIcon = themeToggleBtn.querySelector('.iconLight');
 const darkIcon = themeToggleBtn.querySelector('.iconDark');
+const lightIcon2 = themeToggleBtn2.querySelector('.iconLight');
+const darkIcon2 = themeToggleBtn2.querySelector('.iconDark');
 
 // State
 const theme = localStorage.getItem('theme');
@@ -11,13 +14,17 @@ if (theme === 'dark-mode') {
     document.body.classList.add('dark-mode');
     // switchText.textContent = 'Dark';
     lightIcon.style.display = 'none';
+    lightIcon2.style.display = 'none';
     darkIcon.style.display = 'inline-block';
+    darkIcon2.style.display = 'inline-block';
 } else {
     // Ensure light mode is explicitly set by default
     document.body.classList.remove('dark-mode');
     // switchText.textContent = 'Light';
     darkIcon.style.display = 'none';
+    darkIcon2.style.display = 'none';
     lightIcon.style.display = 'inline-block';
+    lightIcon2.style.display = 'inline-block';
 }
 
 // Handlers
@@ -27,14 +34,19 @@ const handleThemeToggle = () => {
         localStorage.setItem('theme', 'dark-mode');
         // switchText.textContent = 'Dark';
         lightIcon.style.display = 'none';
+        lightIcon2.style.display = 'none';
         darkIcon.style.display = 'inline-block';
+        darkIcon2.style.display = 'inline-block';
     } else {
         localStorage.removeItem('theme');
         // switchText.textContent = 'Light';
         darkIcon.style.display = 'none';
+        darkIcon2.style.display = 'none';
         lightIcon.style.display = 'inline-block';
+        lightIcon2.style.display = 'inline-block';
     }
 };
 
 // Events
 themeToggleBtn.addEventListener('click', handleThemeToggle);
+themeToggleBtn2.addEventListener('click', handleThemeToggle);
