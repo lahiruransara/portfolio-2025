76dba968b83892c790b1e483c77dfe1fe7ecd597
Lahiru Ransara <lahiruransara123@gmail.com>
2025-11-13 09:24:09 -0800

    background shader for index page

--- js/mobileNavBtn.js
+++ js/mobileNavBtn.js
@@ -1,7 +1,7 @@
 //Custom JS
 $(document).ready(function(){
 	$('a.target-burger').click(function(e){
-		$('div.blur-container, nav.main-nav, a.target-burger').toggleClass('toggled');
+		$('.blur-container, nav.main-nav, a.target-burger').toggleClass('toggled');
 		e.preventDefault();
 	});//target-burger-click
 });//doc-rdy
